c54986e01e7f081322e8801e9226a63575bb82ca
Yonghui Dong <yonghui.dong@gmail.com>
2021-07-19 06:50:06 +0000

    version 0.1.0

---
+++ R/runGui.R
@@ -0,0 +1,19 @@
+#' @title Run RawHummus Gui
+#' @description Run RawHummus Gui
+#' @author Yonghui Dong
+#' @import shiny shinydashboardPlus shinyFiles shinyMatrix shinyvalidate shinycustomloader RaMS purrr dplyr plotly RColorBrewer rmarkdown
+#' @importFrom shinydashboard sidebarMenu
+#' @importFrom DT selectCells
+#' @export
+#' @return Gui
+#' @examples
+#' if(interactive()){}
+
+runGui <- function() {
+  appDir <- system.file("shiny", "Gui", package = "RawHummus")
+  if (appDir == "") {
+    stop("Could not find Shiny Gui directory. Try re-installing `RawHummus`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
